38f627b6eac8c4c96115cec67a727f715f82bbc9
BenBilbro <benb16@vt.edu>
2017-04-12 19:42:37 -0400

    Add files via upload

--- Project 5/src/prj5/LinkedList.java
+++ Project 5/src/prj5/LinkedList.java
@@ -8,5 +8,59 @@ package prj5;
  *
  */
 public class LinkedList<T> {
+    private int length;
+    private Node<T> head;
 
+
+    public LinkedList() {
+        length = 0;
+        head = null;
+    }
+
+
+    public int getLength() {
+        return length;
+    }
+
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+
+    private class Node<T> {
+        private T data;
+        private Node<T> next;
+
+
+        private Node(T dataPortion) {
+            this(null, dataPortion);
+        }
+
+
+        private Node(Node<T> nextNode, T newData) {
+            data = newData;
+            next = nextNode;
+        }
+
+
+        private void setData(T newData) {
+            data = newData;
+        }
+
+
+        private T getData() {
+            return data;
+        }
+
+
+        private void setNext(Node<T> nextNode) {
+            next = nextNode;
+        }
+
+
+        private Node<T> getNext() {
+            return next;
+        }
+    }
 }
--- Project 5/src/prj5/Song.java
+++ Project 5/src/prj5/Song.java
@@ -8,5 +8,31 @@ package prj5;
  *
  */
 public class Song {
+    
+    private String artist;
+    private String genre;
+    private String title;
+    private String date;
+    private LinkedList<Person> heardList;
+    private LinkedList<Person> likedList;
+    
 
+    public static void main(String[] args) {
+        
+    }
+
+
+    public static int[] getResults(Class<?> enumClass) {
+        if (enumClass.equals(Hobby.class)) {
+            
+        }
+        else if (enumClass.equals(Region.class)) {
+            
+        }
+        else if (enumClass.equals(Major.class)) {
+            
+        }
+        
+        return new int[0];
+    }
 }
